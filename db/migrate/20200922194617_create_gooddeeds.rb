1cdc542f2b3d8e5ec1e51b29702016d1d2da0c68
github username <github email address>
2020-09-22 21:09:02 -0400

    added to good deeds

--- db/migrate/20200922194617_create_gooddeeds.rb
+++ db/migrate/20200922194617_create_gooddeeds.rb
@@ -1,7 +1,10 @@
 class CreateGooddeeds < ActiveRecord::Migration[6.0]
   def change
     create_table :gooddeeds do |t|
-      t.string :body 
+      t.string :body
+      t.string :name
+      t.string :location 
+      t.string :feeling
       t.datetime :day 
       t.timestamps
     end
